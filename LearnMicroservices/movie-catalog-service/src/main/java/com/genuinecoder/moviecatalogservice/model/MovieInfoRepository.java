bb72c7a98d4a7b7ed347e7ac5fb70e17f9cb70d8
shubhamtribhuwan171 <133274259+shubhamtribhuwan171@users.noreply.github.com>
2024-10-17 02:50:17 +0530

    Delete LearnMicroservices directory

--- LearnMicroservices/movie-catalog-service/src/main/java/com/genuinecoder/moviecatalogservice/model/MovieInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.genuinecoder.moviecatalogservice.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieInfoRepository extends JpaRepository<MovieInfo, Long> {
-
-}
